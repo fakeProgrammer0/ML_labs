c7cc924d7537c2a789b421abb80cd0264434823c
fakeProgrammer0 <greenjiachen@gmail.com>
2018-10-26 23:27:56 +0800

    update SVM_SGD

--- lab2/SVM_SGD.py
+++ lab2/SVM_SGD.py
@@ -15,20 +15,20 @@ n_features = 123
 # --------------------------
 
 import numpy as np
-import math
 import matplotlib.pyplot as plt
 import random
 
 from sklearn.datasets import load_svmlight_file
+from sklearn.metrics import f1_score
 
 def preprocess(dataset_url, n_features):
     X, y = load_svmlight_file(dataset_url, n_features=n_features)
 
-    X = X.toarray()
-    X = np.hstack((np.ones((X.shape[0], 1)), X))
-
     y = y.reshape(-1, 1)
     # y = y.reshape(-1, 1)[:, 0] # change y into a 1D ndarray
+
+    X = X.toarray()
+    X = np.hstack((np.ones(y.shape), X))
 
     return X, y
 
@@ -51,6 +51,12 @@ def svm_SGD(X_train, y_train, X_val, y_val, batch_size=100, max_epoch=200, learn
         else:
             return -1
 
+    def sign_col_vector(a):
+        n = a.shape[0]
+        for i in range(0, n):
+            a[i][0] = sign(a[i][0])
+        return a
+
     n_train_samples, n_features = X_train.shape
 
     if batch_size > n_train_samples:
@@ -62,6 +68,9 @@ def svm_SGD(X_train, y_train, X_val, y_val, batch_size=100, max_epoch=200, learn
     losses_train = []
     losses_val = []
 
+    f1_scores_train = []
+    f1_scores_val = []
+
     for epoch in range(0, max_epoch):
         sample_indice = random.sample(range(0, n_train_samples), batch_size)
         temp_sum = np.zeros(w.shape)
@@ -69,25 +78,32 @@ def svm_SGD(X_train, y_train, X_val, y_val, batch_size=100, max_epoch=200, learn
             if 1 - y_train[i][0] * np.dot(X_train[i], w)[0] > 0:
                 temp_sum += -y_train[i][0] * X_train[i].reshape(-1, 1)
 
-        w = (1 - reg_param) * w - penalty_factor / batch_size * temp_sum
+        w = (1 - reg_param * learning_rate) * w - learning_rate * penalty_factor / batch_size * temp_sum
 
         loss_train = hinge_loss(X_train, y_train, w)
         loss_val = hinge_loss(X_val, y_val, w)
-
         losses_train.append(loss_train)
-        losses_val.append(losses_val)
+        losses_val.append(loss_val)
+        print("epoch [%3d]: loss_train = [%.6f]; loss_val = [%.6f]" % (epoch, loss_train, loss_val))
 
-        print("epoch [%4d]: loss_train = [%.6f]; loss_val = [%.6f]" % (epoch, loss_train, loss_val))
+        y_train_predict = sign_col_vector(np.dot(X_train, w)).reshape(n_train_samples)
+        y_val_predict = sign_col_vector(np.dot(X_val, w)).reshape(X_val.shape[0])
+        f1_train = f1_score(y_true=y_train, y_pred=y_train_predict)
+        f1_val = f1_score(y_true=y_val, y_pred=y_val_predict)
+        f1_scores_train.append(f1_train)
+        f1_scores_val.append(f1_val)
 
-    return w, losses_train, losses_val
+        print("epoch [%3d]: f1_train = [%.6f]; f1_val = [%.6f]\n" % (epoch, f1_train, f1_val))
+
+    return w, losses_train, losses_val, f1_scores_train, f1_scores_val
 
 def hinge_loss(X, y, w):
-    return np.average(np.maximum(np.ones(y.shape) - y * np.dot(X, w), np.zeros(y.shape)), axis=0)
+    return np.average(np.maximum(np.ones(y.shape) - y * np.dot(X, w), np.zeros(y.shape)), axis=0)[0]
 
 def run_svm():
     X_train, y_train = preprocess(train_dataset_url, n_features)
     X_val, y_val = preprocess(val_dataset_url, n_features)
-    w, losses_train, losses_val = svm_SGD(X_train, y_train, X_val, y_val)
+    w, losses_train, losses_val, f1_scores_train, f1_scores_val = svm_SGD(X_train, y_train, X_val, y_val, max_epoch=200, batch_size=100, learning_rate=0.1)
 
     plt.figure(figsize=(16, 9))
     plt.plot(losses_train, '-', color='r', label='losses_train')
@@ -98,5 +114,28 @@ def run_svm():
     plt.title('loss graph of svm')
     plt.show()
 
+    plt.figure(figsize=(16, 9))
+    plt.plot(f1_scores_train, '-', color='r', label='f1_scores_train')
+    plt.plot(f1_scores_val, '-', color='b', label='f1_scores_val')
+    plt.xlabel('epoch')
+    plt.ylabel('f1_score')
+    plt.legend()
+    plt.title('f1_scores graph of svm')
+    plt.show()
+
+from sklearn.svm import SVC
+def check_svm():
+    X_train, y_train = preprocess(train_dataset_url, n_features)
+    X_val, y_val = preprocess(val_dataset_url, n_features)
+    clf = SVC(gamma='auto')
+    clf.fit(X_train, y_train)
+
+    y_train_predict = clf.predict(X_train)
+    y_val_predict = clf.predict(X_val)
+    f1_train = f1_score(y_true=y_train, y_pred=y_train_predict)
+    f1_val = f1_score(y_true=y_val, y_pred=y_val_predict)
+    print("f1_train = [%.6f]; f1_val = [%.6f]\n" % (f1_train, f1_val))
+
 if __name__ == "__main__":
     run_svm()
+    # check_svm()
