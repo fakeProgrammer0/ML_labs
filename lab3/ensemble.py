a6f6628b6f059ed490b47cc280feeb349ff42628
fakeProgrammer0 <greenjiachen@gmail.com>
2018-11-11 20:09:19 +0800

    adaboost 1st commit

--- lab3/ensemble.py
+++ lab3/ensemble.py
@@ -1,7 +1,11 @@
 import pickle
+import numpy as np
+import math
 
 class AdaBoostClassifier:
-    '''A simple AdaBoost Classifier.'''
+    '''A simple AdaBoost Classifier.
+    Only support binary classification currently.
+    '''
 
     def __init__(self, weak_classifier, n_weakers_limit):
         '''Initialize AdaBoostClassifier
@@ -10,6 +14,9 @@ class AdaBoostClassifier:
             weak_classifier: The class of weak classifier, which is recommend to be sklearn.tree.DecisionTreeClassifier.
             n_weakers_limit: The maximum number of weak classifier the model can use.
         '''
+        self.weak_clf = weak_classifier
+        self.n_weakers = n_weakers_limit
+
         pass
 
     def is_good_enough(self):
@@ -23,11 +30,32 @@ class AdaBoostClassifier:
             X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
             y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
         '''
+        n_samples, n_features = X.shape
+        w = np.ones(y.shape)
 
+        self.a = np.zeros(self.n_weakers)
+        self.clfs = []
 
+        for i in range(self.n_weakers):
+            base_clf = self.weak_clf()
+            base_clf.fit(X, y, w)
+            self.clfs.append(base_clf)
 
+            y_pred = base_clf.predict(X)
 
-        pass
+            # can be optimized into more clean and simple code
+            # temp = np.ones(y.shape)
+            # for j in range(n_samples):
+            #     if y_pred[j][0] == y[j][0]:
+            #         temp[j][0] = 0
+            #
+            # err_rate = w.dot(temp)[0] / w.sum()
+
+            err_rate = w.dot(y_pred==y)[0] / w.sum()
+
+            self.a[i] = math.log((1 - err_rate) / err_rate) / 2
+
+            w = w * np.exp(-self.a[i] * y * y_pred)
 
 
     def predict_scores(self, X):
@@ -39,10 +67,14 @@ class AdaBoostClassifier:
         Returns:
             An one-dimension ndarray indicating the scores of differnt samples, which shape should be (n_samples,1).
         '''
-        pass
+        y_score_pred = np.shape((X.shape[0], 1))
+        for i in range(self.n_weakers):
+            y_score_pred += self.a[i] * self.clfs[i].predict(X)
+
+        return y_score_pred
 
     def predict(self, X, threshold=0):
-        '''Predict the catagories for geven samples.
+        '''Predict the catagories for given samples.
 
         Args:
             X: An ndarray indicating the samples to be predicted, which shape should be (n_samples,n_features).
@@ -51,7 +83,18 @@ class AdaBoostClassifier:
         Returns:
             An ndarray consists of predicted labels, which shape should be (n_samples,1).
         '''
-        pass
+        def my_sign(a, threshold=0, sign_threshold=1):
+            if sign_threshold != 1 or sign_threshold != -1:
+                raise ValueError('sign_threshold must be -1 or 1')
+            if not (-1 < threshold < 1):
+                raise ValueError('threshold must be between -1 and 1')
+            sign_a = np.zeros(a.shape)
+            sign_a += a > threshold
+            sign_a -= a < threshold
+            sign_a += sign_threshold * (a == threshold)
+            return sign_a
+
+        return my_sign(self.predict_scores(X),threshold=threshold)
 
     @staticmethod
     def save(model, filename):
--- lab3/train.py
+++ lab3/train.py
@@ -1,6 +1,12 @@
+from sklearn.tree import DecisionTreeClassifier
 
+from .ensemble import AdaBoostClassifier
 
 if __name__ == "__main__":
     # write your code here
+
+    clf = AdaBoostClassifier(DecisionTreeClassifier, 10)
+
+
     pass
 
